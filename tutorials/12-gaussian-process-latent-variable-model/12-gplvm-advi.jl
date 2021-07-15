84d74aacf585fcb33558edd6be6b3c7b15dbe191
leachim <leachim@users.noreply.github.com>
2021-07-15 11:39:26 +0100

    mu

--- tutorials/12-gaussian-process-latent-variable-model/12-gplvm-advi.jl
+++ tutorials/12-gaussian-process-latent-variable-model/12-gplvm-advi.jl
@@ -1,7 +1,7 @@
 # Load Turing.
 using Turing
 #  using AbstractGPs, Plots
-using AbstractGPs, Stheno, KernelFunctions, Random, Plots
+using AbstractGPs, KernelFunctions, Random, Plots
 
 # Background reading:
 # https://jmlr.org/papers/v6/lawrence05a.html
@@ -42,13 +42,17 @@ sekernel(α, σ) = SqExponentialKernel() ∘ ARDTransform(α)
   Z ~ filldist(Normal(0., 1.), K, N)
 
   kernel = sekernel(α, σ[1])
+  K = kernelmatrix(kernel, Z)  # cov matrix
+  K += LinearAlgebra.I * (σ[1] + noise)
+
+  Y ~ filldist(MvNormal(zeros(N), K), D)
 
   ## DENSE GP
-  gp = GP(kernel, GPC())
+  #  gp = GP(kernel, GPC())
   #  prior = gp(ColVecs(Z), noise)
-  prior = gp(ColVecs(Z))
+  #  prior = gp(ColVecs(Z))
 
-  Y ~ filldist(prior, D)
+  #  Y ~ filldist(prior, D)
 end
 
 Y = oil_matrix
@@ -69,50 +73,51 @@ ndim=4
 #  Y ~ filldist(prior, N, D)
 # END DEBUG CODE
 
-n_data=40
+n_data=100
 gplvm = GPLVM(oil_matrix[1:n_data,:], ndim)
 
 #  # takes a while
-#  chain = sample(gplvm, NUTS(), 300)
-#  z_mean = permutedims(reshape(mean(group(chain, :Z))[:,2], (ndim, n_data)))'
-#  alpha_mean = mean(group(chain, :α))[:,2]
-#  #
-#  df_gplvm = DataFrame(z_mean', :auto)
-#  rename!(df_gplvm, Symbol.( ["z"*string(i) for i in collect(1:ndim)]))
-#  df_gplvm[!,:sample] = 1:n_data
-#  df_gplvm[!,:labels] = labels[1:n_data]
-#  df_gplvm|>  @vlplot(:point, x=:z1, y=:z2, color="labels:n")
-#  #
-#  alpha_indices = sortperm(alpha_mean)[1:2]
-#  df_gplvm[!,:ard1] = z_mean[alpha_indices[1], :]
-#  df_gplvm[!,:ard2] = z_mean[alpha_indices[2], :]
-#  df_gplvm |>  @vlplot(:point, x=:ard1, y=:ard2, color="labels:n")
+chain = sample(gplvm, NUTS(), 500)
+z_mean = permutedims(reshape(mean(group(chain, :Z))[:,2], (ndim, n_data)))'
+#  z_mean = reshape(mean(group(chain, :Z))[:,2], (n_data, ndim))'
+alpha_mean = mean(group(chain, :α))[:,2]
 
-advi = ADVI(10, 1000)
-q = vi(gplvm, advi);
+df_gplvm = DataFrame(z_mean', :auto)
+rename!(df_gplvm, Symbol.( ["z"*string(i) for i in collect(1:ndim)]))
+df_gplvm[!,:sample] = 1:n_data
+df_gplvm[!,:labels] = labels[1:n_data]
+df_gplvm|>  @vlplot(:point, x=:z1, y=:z2, color="labels:n")
 
-z_post = rand(q, 10000);
+alpha_indices = sortperm(alpha_mean)[1:2]
+df_gplvm[!,:ard1] = z_mean[alpha_indices[1], :]
+df_gplvm[!,:ard2] = z_mean[alpha_indices[2], :]
+df_gplvm |>  @vlplot(:point, x=:ard1, y=:ard2, color="labels:n")
 
-#  awkward way to extract variables
-_, sym2range = bijector(gplvm, Val(true));
-sym2range
+#  advi = ADVI(10, 1000)
+#  q = vi(gplvm, advi);
 
-α_mean = mean(z_post[union(sym2range[:α]...),:], dims=2)
+#  z_post = rand(q, 10000);
 
-Z_mean = reshape(mean(z_post[union(sym2range[:Z]...),:], dims=2), n_data, :)
-
-df_in = DataFrame(oil_matrix[1:n_data,:], :auto)
-df_in[!,:sample] = 1:n_data
-df_in[!,:labels] = labels[1:n_data]
-df_latent = DataFrames.DataFrame(Z_mean, ["z" * string(i) for i in 1:size(Z_mean)[2]])
-df_oil = hcat(df_in, df_latent)
-
-alpha_indices = sortperm(α_mean[:,1])[1:2]
-p1 = df_oil |>  @vlplot(:point, x="z"*string(alpha_indices[1]), y="z"*string(alpha_indices[2]), color="labels:n")
-
-p2 = df_oil |>  @vlplot(:point, x="z1", y="z2", color="labels:n")#, shape=:batch)
-p1
-p2
+#  #  awkward way to extract variables
+#  _, sym2range = bijector(gplvm, Val(true));
+#  sym2range
+#
+#  α_mean = mean(z_post[union(sym2range[:α]...),:], dims=2)
+#
+#  Z_mean = reshape(mean(z_post[union(sym2range[:Z]...),:], dims=2), n_data, :)
+#
+#  df_in = DataFrame(oil_matrix[1:n_data,:], :auto)
+#  df_in[!,:sample] = 1:n_data
+#  df_in[!,:labels] = labels[1:n_data]
+#  df_latent = DataFrames.DataFrame(Z_mean, ["z" * string(i) for i in 1:size(Z_mean)[2]])
+#  df_oil = hcat(df_in, df_latent)
+#
+#  alpha_indices = sortperm(α_mean[:,1])[1:2]
+#  p1 = df_oil |>  @vlplot(:point, x="z"*string(alpha_indices[1]), y="z"*string(alpha_indices[2]), color="labels:n")
+#
+#  p2 = df_oil |>  @vlplot(:point, x="z1", y="z2", color="labels:n")#, shape=:batch)
+#  p1
+#  p2
 
 #  DataFrames.stack(df_oil, 1:2) |>
     #  @vlplot(:rect, x="sample:o", color=:value, encoding={y={field="variable", type="nominal", sort="-x", axis={title="gene"}}})
